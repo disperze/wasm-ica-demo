969690486d576fd80fea2ed7645835b795cba944
Giancarlos Salas <me@giansalex.dev>
2022-08-19 12:34:54 -0500

    Fix wasm portID

--- x/inter-tx/keeper/msg_server.go
+++ x/inter-tx/keeper/msg_server.go
@@ -27,6 +27,8 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 func (k msgServer) RegisterAccount(goCtx context.Context, msg *types.MsgRegisterAccount) (*types.MsgRegisterAccountResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	// TODO: verify contract address supports IBC
+	// wasmPort := "wasm." + msg.Owner
 	if err := k.icaControllerKeeper.RegisterInterchainAccount(ctx, msg.ConnectionId, msg.Owner, msg.Version); err != nil {
 		return nil, err
 	}
