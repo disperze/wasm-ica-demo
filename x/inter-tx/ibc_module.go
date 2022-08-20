d9ecfb35e7d72e1d4ded1a5c90020f96e2e5ff4c
Giancarlos Salas <me@giansalex.dev>
2022-08-19 22:34:43 -0500

    Add wasm ica-auth

--- x/inter-tx/ibc_module.go
+++ x/inter-tx/ibc_module.go
@@ -1,10 +1,10 @@
 package inter_tx
 
 import (
-	"context"
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
@@ -12,22 +12,27 @@ import (
 	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
 	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
 	"github.com/cosmos/interchain-accounts/x/inter-tx/keeper"
+
+	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 )
 
 var _ porttypes.IBCModule = IBCModule{}
-var icaVal string = "ica"
 
 // IBCModule implements the ICS26 interface for interchain accounts controller chains
 type IBCModule struct {
-	app    porttypes.IBCModule
-	keeper keeper.Keeper
+	keeper        keeper.Keeper
+	wasm          wasmtypes.IBCContractKeeper
+	channelKeeper wasmtypes.ChannelKeeper
 }
 
 // NewIBCModule creates a new IBCModule given the keeper
-func NewIBCModule(app porttypes.IBCModule, k keeper.Keeper) IBCModule {
+func NewIBCModule(keeper keeper.Keeper, wasmKeeper wasmtypes.IBCContractKeeper, chanKeeper wasmtypes.ChannelKeeper) IBCModule {
 	return IBCModule{
-		app:    app,
-		keeper: k,
+		keeper:        keeper,
+		wasm:          wasmKeeper,
+		channelKeeper: chanKeeper,
 	}
 }
 
@@ -42,10 +47,30 @@ func (im IBCModule) OnChanOpenInit(
 	counterparty channeltypes.Counterparty,
 	version string,
 ) error {
-	// id for ica calls for skip wasm.ClaimCapability
-	icaCtx := ctx.WithContext(context.WithValue(ctx.Context(), "ica", &icaVal))
-	wasmPortID := strings.Replace(portID, icatypes.PortPrefix, "wasm.", 1)
-	if err := im.app.OnChanOpenInit(icaCtx, order, connectionHops, wasmPortID, channelID, chanCap, counterparty, version); err != nil {
+	if err := wasm.ValidateChannelParams(channelID); err != nil {
+		return err
+	}
+
+	contractAddr, err := ContractFromPortID(portID)
+	if err != nil {
+		return sdkerrors.Wrapf(err, "contract port id")
+	}
+
+	msg := wasmvmtypes.IBCChannelOpenMsg{
+		OpenInit: &wasmvmtypes.IBCOpenInit{
+			Channel: wasmvmtypes.IBCChannel{
+				Endpoint:             wasmvmtypes.IBCEndpoint{PortID: portID, ChannelID: channelID},
+				CounterpartyEndpoint: wasmvmtypes.IBCEndpoint{PortID: counterparty.PortId, ChannelID: counterparty.ChannelId},
+				Order:                order.String(),
+				// DESIGN V3: this may be "" ??
+				Version:      version,
+				ConnectionID: connectionHops[0], // At the moment this list must be of length 1. In the future multi-hop channels may be supported.
+			},
+		},
+	}
+
+	_, err = im.wasm.OnOpenChannel(ctx, contractAddr, msg)
+	if err != nil {
 		return err
 	}
 
@@ -80,10 +105,28 @@ func (im IBCModule) OnChanOpenAck(
 	counterpartyChannelID string,
 	counterpartyVersion string,
 ) error {
-	// wasm call SetChannelID
-	icaCtx := ctx.WithContext(context.WithValue(ctx.Context(), "ica", &icaVal))
-	wasmPortID := strings.Replace(portID, icatypes.PortPrefix, "wasm.", 1)
-	return im.app.OnChanOpenAck(icaCtx, wasmPortID, channelID, counterpartyChannelID, counterpartyVersion)
+	contractAddr, err := ContractFromPortID(portID)
+	if err != nil {
+		return sdkerrors.Wrapf(err, "contract port id")
+	}
+	channelInfo, ok := im.channelKeeper.GetChannel(ctx, portID, channelID)
+	if !ok {
+		return sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", portID, channelID)
+	}
+	channelInfo.Counterparty.ChannelId = counterpartyChannelID
+	// This is a bit ugly, but it is set AFTER the callback is done, yet we want to provide the contract
+	// access to the channel in queries. We can revisit how to better integrate with ibc-go in the future,
+	// but this is the best/safest we can do now. (If you remove this, you error when sending a packet during the
+	// OnChanOpenAck entry point)
+	// https://github.com/cosmos/ibc-go/pull/647/files#diff-54b5be375a2333c56f2ae1b5b4dc13ac9c734561e30286505f39837ee75762c7R25
+	im.channelKeeper.SetChannel(ctx, portID, channelID, channelInfo)
+	msg := wasmvmtypes.IBCChannelConnectMsg{
+		OpenAck: &wasmvmtypes.IBCOpenAck{
+			Channel:             toWasmVMChannel(portID, channelID, channelInfo),
+			CounterpartyVersion: counterpartyVersion,
+		},
+	}
+	return im.wasm.OnConnectChannel(ctx, contractAddr, msg)
 }
 
 // OnChanOpenConfirm implements the IBCModule interface
@@ -135,9 +178,20 @@ func (im IBCModule) OnAcknowledgementPacket(
 	acknowledgement []byte,
 	relayer sdk.AccAddress,
 ) error {
-	icaCtx := ctx.WithContext(context.WithValue(ctx.Context(), "ica", &icaVal))
-	packet.SourcePort = strings.Replace(packet.SourcePort, icatypes.PortPrefix, "wasm.", 1)
-	return im.app.OnAcknowledgementPacket(icaCtx, packet, acknowledgement, relayer)
+	contractAddr, err := ContractFromPortID(packet.SourcePort)
+	if err != nil {
+		return sdkerrors.Wrapf(err, "contract port id")
+	}
+
+	err = im.wasm.OnAckPacket(ctx, contractAddr, wasmvmtypes.IBCPacketAckMsg{
+		Acknowledgement: wasmvmtypes.IBCAcknowledgement{Data: acknowledgement},
+		OriginalPacket:  newIBCPacket(packet),
+		Relayer:         relayer.String(),
+	})
+	if err != nil {
+		return sdkerrors.Wrap(err, "on ack")
+	}
+	return nil
 }
 
 // OnTimeoutPacket implements the IBCModule interface.
@@ -146,7 +200,51 @@ func (im IBCModule) OnTimeoutPacket(
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) error {
-	icaCtx := ctx.WithContext(context.WithValue(ctx.Context(), "ica", &icaVal))
-	packet.SourcePort = strings.Replace(packet.SourcePort, icatypes.PortPrefix, "wasm.", 1)
-	return im.app.OnTimeoutPacket(icaCtx, packet, relayer)
+	contractAddr, err := ContractFromPortID(packet.SourcePort)
+	if err != nil {
+		return sdkerrors.Wrapf(err, "contract port id")
+	}
+	msg := wasmvmtypes.IBCPacketTimeoutMsg{Packet: newIBCPacket(packet), Relayer: relayer.String()}
+	err = im.wasm.OnTimeoutPacket(ctx, contractAddr, msg)
+	if err != nil {
+		return sdkerrors.Wrap(err, "on timeout")
+	}
+	return nil
+}
+
+func ContractFromPortID(portID string) (sdk.AccAddress, error) {
+	if !strings.HasPrefix(portID, icatypes.PortPrefix) {
+		return nil, sdkerrors.Wrapf(wasmtypes.ErrInvalid, "without prefix")
+	}
+	return sdk.AccAddressFromBech32(portID[len(icatypes.PortPrefix):])
+}
+
+func newIBCPacket(packet channeltypes.Packet) wasmvmtypes.IBCPacket {
+	timeout := wasmvmtypes.IBCTimeout{
+		Timestamp: packet.TimeoutTimestamp,
+	}
+	if !packet.TimeoutHeight.IsZero() {
+		timeout.Block = &wasmvmtypes.IBCTimeoutBlock{
+			Height:   packet.TimeoutHeight.RevisionHeight,
+			Revision: packet.TimeoutHeight.RevisionNumber,
+		}
+	}
+
+	return wasmvmtypes.IBCPacket{
+		Data:     packet.Data,
+		Src:      wasmvmtypes.IBCEndpoint{ChannelID: packet.SourceChannel, PortID: packet.SourcePort},
+		Dest:     wasmvmtypes.IBCEndpoint{ChannelID: packet.DestinationChannel, PortID: packet.DestinationPort},
+		Sequence: packet.Sequence,
+		Timeout:  timeout,
+	}
+}
+
+func toWasmVMChannel(portID, channelID string, channelInfo channeltypes.Channel) wasmvmtypes.IBCChannel {
+	return wasmvmtypes.IBCChannel{
+		Endpoint:             wasmvmtypes.IBCEndpoint{PortID: portID, ChannelID: channelID},
+		CounterpartyEndpoint: wasmvmtypes.IBCEndpoint{PortID: channelInfo.Counterparty.PortId, ChannelID: channelInfo.Counterparty.ChannelId},
+		Order:                channelInfo.Ordering.String(),
+		Version:              channelInfo.Version,
+		ConnectionID:         channelInfo.ConnectionHops[0], // At the moment this list must be of length 1. In the future multi-hop channels may be supported.
+	}
 }
